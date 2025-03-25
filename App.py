1d05429be41387dc6d86b9f5eaa8ef6759abb443
revanth <revanth44445@gmail.com>
2025-03-25 14:13:07 +0530

    fix: Optimized code blocks

--- App.py
+++ App.py
@@ -1,44 +1,105 @@
 from docx.api import Document
 from docx.shared import Pt
-
-def listFormat(para):
-    para=para.replace("*","")
-    return para
-
-def appRun():
-    markdown_content=""
-    doc=Document('demo.docx')
-    heading_flag=False
-    mainhead_flag=False
-    for para in doc.paragraphs:
-        for run in para.runs:
-            if (run.font.size == Pt(14)):
-                heading_flag = True
-                # print("changing flag" , heading_flag)
-                break
-            else:
-                heading_flag=False
-            break
+import sys
+import os
 
 
-        if heading_flag:
-            if mainhead_flag:
-                markdown_content+="## "+para.text+"\n"
-            else:
-                markdown_content+="# "+para.text+"\n"
-                mainhead_flag=True
+global markdown_content
 
-        if heading_flag!=True and not para.text.strip().startswith('*'):
-            markdown_content+=para.text+"\n\n"
+print("Ensure program folder contains demo.docx and delete README.md to avoid confusion \n")
 
-        if heading_flag!=True and para.text.strip().startswith('*') or para.style.name==('List Bullet') or para.text.startswith('•'):
-            markdown_content+="* "+listFormat(para.text)+"\n\n"
-    print(markdown_content)
+def checkfile(doc_path):
+    return os.path.isfile(doc_path)
+
+def initialize():
+    doc=None
+    if(len(sys.argv)>1):
+        print("Selected "+sys.argv[1]+" File")
+        doc=Document(sys.argv[1])
+    else:
+        if(checkfile('demo.docx')):
+            doc=Document('demo.docx')
+            print("converting default file into ReadMe.md")
+        else:
+            print('end of the line')
+            print('retry with a new demo.docx file in root folder of this program or \n paste a docx file and re-run App.py with argument folder name')
+            exit()
+    
+    return doc
+
+def addMainHeading(paragraph):
+    return "# "+paragraph.text+"\n\n"
+
+def addHeading(paragraph):
+    return "## "+paragraph.text+"\n"
+
+def checkBullets(paragraph):
+    flag =False
+    if(paragraph.text.strip().startswith('*')):
+        flag=True
+    else:
+        flag=False
+    return flag
+
+def formateList(paragraph):
+    temp=paragraph.text
+    if(temp.strip().startswith("* " or "*")):
+        temp=temp.replace("*","")
+        temp="* "+temp+"\n"
+    elif(paragraph.style.name=="List Bullet"):
+        temp=paragraph.text+"\n"
+    return temp
+
+def saveMd(content):
     try:
-        with open('README.md', 'w') as mdFile:
-            mdFile.write(markdown_content)
-        print("ReadMe file write success")
+        file_output=open("README.md",'w')
+        file_output.write(markdown_content)
+        file_output.close()
+        print("Write readme file success")
     except Exception as e:
         print(e)
+    
+def checkHeading(run):
+    flag=False
+    if(run.font.size==Pt(14)):
+        flag=True
+    else:
+        flag=False
+    return flag
 
-appRun()
+def format_md_content(doc):
+    heading =False
+    main_heading=False
+    for paragraphs in doc.paragraphs:
+        # print(paragraphs.text)
+        for run in paragraphs.runs:
+            if(checkHeading(run)):
+                heading=True
+            else:
+                heading=False
+        
+        if heading:
+            if main_heading:
+                markdown_content+=addHeading(paragraphs)
+            else:
+                markdown_content=addMainHeading(paragraphs)
+                main_heading=True
+        else:
+            # markdown_content+=paragraphs.text+"\n"
+            if checkBullets(paragraphs):
+                markdown_content+=formateList(paragraphs)
+            else:
+                markdown_content+=paragraphs.text+"\n"
+
+    # print(markdown_content)
+    return markdown_content
+
+
+def __init__(self):
+    print("App is started running")
+    self.format_md_content()
+    
+
+markdown_content=format_md_content(initialize())
+# print(markdown_content)
+saveMd(markdown_content)
